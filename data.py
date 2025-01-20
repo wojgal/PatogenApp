34f08c00cc44968c194d7bd2fc5fa602080527fc
wojgal <wojgal21@gmail.com>
2025-01-20 17:36:25 +0100

    dodanie pdf

--- data.py
+++ data.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import streamlit as st
+from reportlab.lib.pagesizes import A4
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image
+from reportlab.lib.styles import getSampleStyleSheet
+import io
 
 def load_data(csv_path='data/patogeny.csv'):
     try:
@@ -9,3 +13,22 @@ def load_data(csv_path='data/patogeny.csv'):
     except FileNotFoundError:
         st.error(f'Plik {csv_path} z danymi nie istnieje.')
         return None
+    
+def create_pdf(pathogen_name_pl, pathogen_name_lat, pathogen_description, therapy_description):
+    buffer = io.BytesIO()
+    document = SimpleDocTemplate(buffer, pagesize=A4)
+    elements = []
+    styles = getSampleStyleSheet()
+
+    elements.append(Paragraph(pathogen_name_pl, styles['BodyText']))
+    elements.append(Spacer(1, 10))
+    elements.append(Paragraph(pathogen_name_lat, styles['BodyText']))
+    elements.append(Spacer(1, 10))
+    elements.append(Paragraph(pathogen_description, styles['BodyText']))
+    elements.append(Spacer(1, 10))
+    elements.append(Paragraph(therapy_description, styles['BodyText']))
+
+    document.build(elements)
+    buffer.seek(0)
+
+    return buffer
--- main.py
+++ main.py
@@ -1,11 +1,45 @@
-# main.py
-import sys
-import streamlit.web.cli as stcli
+import streamlit as st
+import base64
+from data import load_data, create_pdf
 
-def main():
-    # symulujemy "streamlit run app.py":
-    sys.argv = ["streamlit", "run", "app.py", "--server.port=8502"]
-    stcli.main()
+st.title('Baza patogenow')
 
-if __name__ == "__main__":
-    main()
+df = load_data()
+
+st.write('Wybierz patogen, aby zobaczyć opis')
+
+options = [f'{row["pathogen_name_pl"]} ({row["pathogen_name_lat"]})' for index, row in df.iterrows()]
+options.sort()
+
+selected_option = st.selectbox('Wybierz patogen: ', options, index=None, placeholder='Wybierz patogen')
+create_pdf_button = None
+
+if selected_option:
+    polish_name = selected_option.split(' (')[0]
+    result = df[df['pathogen_name_pl'] == polish_name].iloc[0]
+    
+    if result is not None:
+        st.write('---')
+        st.subheader(f"{result['pathogen_name_pl']} ({result['pathogen_name_lat']})")
+        st.write('---')
+        st.write('**Opis patogenu:**')
+        st.write(result['pathogen_description'])
+        st.write('---')
+        st.write('**Opis terapii:**')
+        st.write(result['therapy_description'])
+    
+    st.write('---')
+    create_pdf_button = st.button('Generuj PDF')
+
+if create_pdf_button:
+    pdf_buffer = create_pdf(result['pathogen_name_pl'], result['pathogen_name_lat'], result['pathogen_description'], result['therapy_description'])
+
+    pdf_base64 = base64.b64encode(pdf_buffer.read()).decode('utf-8')
+    pdf_buffer.seek(0)
+
+    pdf_display = f'<iframe src="data:application/pdf;base64,{pdf_base64}" width="700" height="900" type="application/pdf"></iframe>'
+    st.markdown(pdf_display, unsafe_allow_html=True)
+
+
+
+
